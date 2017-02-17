5cfe689fe03c8b4215b3d117582202dec4b3f4a9
sangclee17 <sangclee17@gmail.com>
2017-02-18 02:14:12 +1100

    Create task.h

---
+++ task.h
@@ -0,0 +1,16 @@
+/***************************************************************************
+ *
+ *   File        : tasks.h
+ *   Name        : Sangchul Lee (Scott)
+ *
+ ***************************************************************************/
+
+#ifndef TASKS_H
+
+void task_1_find_longest_break_after_2k(const char* eq_csv_file);
+
+void task_2_compute_cell_scores(const char* eq_csv_file, int resolution, int K);
+
+void task_3_magnitude_statistics(const char* eq_csv_file);
+
+#endif
